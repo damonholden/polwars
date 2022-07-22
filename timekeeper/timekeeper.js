41461cb39a0a5ccf9f28d1fbfd93bc73a51c88bc
DamonHolden <damonh003@gmail.com>
2022-07-22 21:30:03 +0100

    refactor: removed unecessary methods

--- timekeeper/timekeeper.js
+++ timekeeper/timekeeper.js
@@ -1,9 +1,9 @@
 const timeKeeper = (timeA, timeB) => {
   const timeSplit = time => {
     const date = time.substring(0, 10);
-    const hours = time.substring(11, 13);
-    const minutes = time.substring(14, 16);
-    const seconds = time.substring(17, 19);
+    const hours = parseInt(time.substring(11, 13));
+    const minutes = parseInt(time.substring(14, 16));
+    const seconds = parseInt(time.substring(17, 19));
 
     return { date, hours, minutes, seconds };
   };
@@ -11,113 +11,43 @@ const timeKeeper = (timeA, timeB) => {
   const clockIn = timeSplit(timeA);
   const clockOut = timeSplit(timeB);
 
-  const headerRow = [
-    `HOUR_0`,
-    `HOUR_1`,
-    `HOUR_2`,
-    `HOUR_3`,
-    `HOUR_4`,
-    `HOUR_5`,
-    `HOUR_6`,
-    `HOUR_7`,
-    `HOUR_8`,
-    `HOUR_9`,
-    `HOUR_10`,
-    `HOUR_11`,
-    `HOUR_12`,
-    `HOUR_13`,
-    `HOUR_14`,
-    `HOUR_15`,
-    `HOUR_16`,
-    `HOUR_17`,
-    `HOUR_18`,
-    `HOUR_19`,
-    `HOUR_20`,
-    `HOUR_21`,
-    `HOUR_22`,
-    `HOUR_23`,
-  ];
-
-  const testRow = [
-    `2021 - 12 - 24`,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    1459,
-    3600,
-    3600,
-    3600,
-    3600,
-    3600,
-    3600,
-    3600,
-    3600,
-    3230,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
+  const hours = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23,
   ];
 
   const contentRow = [];
 
-  const multiContentRow = [];
-
-  for (header of headerRow) {
-    if (parseInt(header.replace(`HOUR_`, ``)) < parseInt(clockIn.hours)) {
+  for (header of hours) {
+    if (header < clockIn.hours) {
       contentRow.push('0');
-    } else if (
-      parseInt(header.replace(`HOUR_`, ``)) === parseInt(clockIn.hours)
-    ) {
+    } else if (header === clockIn.hours) {
       if (clockIn.hours === clockOut.hours) {
         contentRow.push(
-          (
-            60 * (parseInt(clockOut.minutes) - parseInt(clockIn.minutes)) +
-            (parseInt(clockOut.seconds) - parseInt(clockIn.seconds))
-          ).toString()
+          60 * (clockOut.minutes - clockIn.minutes) +
+            (clockOut.seconds - clockIn.seconds)
         );
       } else {
-        contentRow.push(
-          (
-            60 * (60 - parseInt(clockIn.minutes)) -
-            parseInt(clockIn.seconds)
-          ).toString()
-        );
+        contentRow.push(60 * (60 - clockIn.minutes) - clockIn.seconds);
       }
-    } else if (
-      parseInt(header.replace(`HOUR_`, ``)) < parseInt(clockOut.hours)
-    ) {
+    } else if (header < clockOut.hours) {
       contentRow.push('3600');
-    } else if (
-      parseInt(header.replace(`HOUR_`, ``)) === parseInt(clockOut.hours)
-    ) {
-      contentRow.push(
-        (
-          60 * parseInt(clockOut.minutes) +
-          parseInt(clockOut.seconds)
-        ).toString()
-      );
+    } else if (header === clockOut.hours) {
+      contentRow.push(60 * clockOut.minutes + clockOut.seconds);
     } else contentRow.push('0');
   }
 
   const rows = [
-    [`DATE`, ...headerRow],
-    [testRow],
+    [`DATE`, ...hours.map(header => `HOUR_${header}`)],
     [clockIn.date, ...contentRow],
   ];
 
-  let csvContent =
+  const csvContent =
     'data:text/csv;charset=utf-8,' + rows.map(e => e.join(',')).join('\n');
 
-  var encodedUri = encodeURI(csvContent);
-  // window.open(encodedUri);
+  const encodedUri = encodeURI(csvContent);
+
+  window.open(encodedUri);
 };
 
 timeKeeper(`2021-12-24 08:35:41`, `2021-12-24 08:45:51`);
